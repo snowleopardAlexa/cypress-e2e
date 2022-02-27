2cc5a3757f528cd467a821a979df3d4e9e07686c
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-27 18:00:37 -0500

    check checkbox

--- cypress/integration/interactions.spec.js
+++ cypress/integration/interactions.spec.js
@@ -15,5 +15,18 @@ describe('Basic page interactions', () => {
     it('displays the correct count for the number of selected checkboxes', () => {
         cy.get('[data-cy=box-2-checkboxes] > nth-child(1) input')
           .check()
+
+        cy.get('[data-cy=box-2-selected-count]')  
+          .invoke('text')
+          .should('equal', '1')
+    })
+
+    it('displays the name of the currently selected item', () => {
+        cy.get('[data-cy=box-3-dropdown]')
+           .select('Option Three');
+
+        cy.get('[data-cy=box-3-selected-name]')
+           .invoke('text')
+           .should('equal', 'Option Three')   
     })
 })
